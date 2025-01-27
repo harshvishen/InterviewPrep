9d0ff1841ec69b45c58e5d47a88e0dd44ed601bc
harshvishen <harshvishensingh7171@gmail.com>
2025-01-27 22:14:05 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { VideoComponent } from './pages/video/video.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'home',
+        pathMatch: 'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent,
+        title:'Home'
+    },
+    {
+        path:'projects',
+        component:ProjectsComponent,
+        title:'Angular-Projects'
+    },
+   
+
+];
